7e9f67eeb3ee4f61c2b5b310b39a2baec939677b
reki2000 <2533597+reki2000@users.noreply.github.com>
2022-08-06 21:49:08 +0900

    reverted

--- lib/sound/sound_player.dart
+++ lib/sound/sound_player.dart
@@ -1,72 +1,16 @@
 // Dart imports:
-import 'dart:developer';
 import 'dart:typed_data';
 
 // Project imports:
-import 'package:just_audio/just_audio.dart';
+import 'sound_player_null.dart' if (dart.library.html) 'sound_player_web.dart';
 
-SoundPlayer getSoundPlayerInstance() => SoundPlayer();
+SoundPlayer getSoundPlayerInstance() => SoundPlayerImpl();
 
-class _Source extends StreamAudioSource {
-  List<int> bytes = List<int>.empty(growable: true);
+abstract class SoundPlayer {
+  Future<void> resume();
 
-  void push(List<int> buf) {
-    bytes.addAll(buf);
-  }
-
-  @override
-  Future<StreamAudioResponse> request([int? start, int? end]) async {
-    log("start:$start end:$end");
-    start ??= 0;
-    end ??= bytes.length;
-    final content = bytes.sublist(start, end);
-    bytes = bytes.sublist(end, bytes.length);
-
-    return StreamAudioResponse(
-      sourceLength: null,
-      contentLength: end - start,
-      offset: start,
-      stream: Stream.value(content),
-      contentType: 'audio/raw',
-    );
-  }
-}
-
-class SoundPlayer {
-  static const _outputSampleRate = 44100;
-  static const _bufferLength = 1024 * 10;
-
-  final _player = AudioPlayer();
-  late final _Source _source;
-
-  final _buf = List.filled(_bufferLength, 0);
-  int _bufIndex = 0;
-
-  SoundPlayer() {
-    _source = _Source();
-    _player.setAudioSource(_source);
-  }
-
-  Future<void> resume() async {
-    await _player.play();
-  }
-
-  Future<void> stop() async {
-    await _player.stop();
-  }
+  Future<void> stop();
 
   // 0.0-1.0 float32
-  void push(Float32List input, int inputSampleRate) {
-    final skip = inputSampleRate / _outputSampleRate;
-    double index = 0.0;
-
-    while (index < input.length) {
-      _buf[_bufIndex++] = (input[index.toInt()] * 255).toInt();
-      if (_bufIndex == _buf.length) {
-        _bufIndex = 0;
-        _source.push(_buf);
-      }
-      index += skip;
-    }
-  }
+  void push(Float32List input, int inputSampleRate);
 }
