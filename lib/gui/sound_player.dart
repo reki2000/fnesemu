ffe5436ddb279ea74b9b5506b0039eedfbe9aeb0
reki2000 <reki2000@gmail.com>
2022-08-10 00:13:02 +0900

    use mp_audio_stream plug-in for sound output

--- lib/gui/sound_player.dart
+++ lib/gui/sound_player.dart
@@ -1,30 +1,22 @@
-// Dart imports:
-import 'dart:developer';
 import 'dart:typed_data';
 
-// Project imports:
-import 'ma_stream.dart';
-import 'sound_player.dart';
+import 'package:mp_audio_stream/mp_audio_stream.dart';
 
-class SoundPlayerImpl extends SoundPlayer {
+class SoundPlayer {
   static const _outputSampleRate = 44100;
-  static const _bufferLength = 1024;
+  final _audioStream = getAudioStream()..init(sampleRate: _outputSampleRate);
 
+  static const _bufferLength = 1024;
   final _buf = Float32List(_bufferLength);
   int _bufIndex = 0;
 
-  SoundPlayerImpl() {
-    MAStream.init();
-    log("SoundPlayerMAStream initialized");
-  }
+  Future<void> resume() async => _audioStream.resume();
 
-  @override
-  Future<void> resume() async {}
-
-  @override
   Future<void> stop() async {}
 
-  @override
+  void dispose() => _audioStream.uninit();
+
+  // resample input buffer (Float32 0.0-1.0 890kHz) to 44.1kHz
   void push(Float32List input, int inputSampleRate) {
     final skip = inputSampleRate / _outputSampleRate;
     double index = 0.0;
@@ -33,7 +25,7 @@ class SoundPlayerImpl extends SoundPlayer {
       _buf[_bufIndex++] = input[index.toInt()];
       if (_bufIndex == _buf.length) {
         _bufIndex = 0;
-        MAStream.push(_buf);
+        _audioStream.push(_buf);
       }
       index += skip;
     }
