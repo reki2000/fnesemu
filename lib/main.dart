d1307a6d49eeb90ed1a903bd4005f23a071f6bb3
reki2000 <2533597+reki2000@users.noreply.github.com>
2022-08-12 06:53:37 +0900

    call `ensureInitialized` when use `fromPlatform` before `runApp`

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,8 @@ import 'package:package_info_plus/package_info_plus.dart';
 // Project imports:
 import 'gui/gui.dart';
 
-void main() {
-  runApp(
-    FutureBuilder<PackageInfo>(
-        future: PackageInfo.fromPlatform(),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return MyApp(title: "fnesemu ${snapshot.data!.version}");
-            default:
-              return const SizedBox();
-          }
-        }),
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final packageInfo = await PackageInfo.fromPlatform();
+  runApp(MyApp(title: "fnesemu ${packageInfo.version}"));
 }
