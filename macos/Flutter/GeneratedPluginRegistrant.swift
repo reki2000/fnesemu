3e30064d1f68eb8298072e6c58270757fb47b2a9
reki2000 <2533597+reki2000@users.noreply.github.com>
2023-04-02 10:27:07 +0900

    bump version

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,7 @@
 import FlutterMacOS
 import Foundation
 
-import package_info_plus_macos
+import package_info_plus
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
