8da40bd0b6b6caba9a1750e16800c0eeeaa48b8d
reki2000 <2533597+reki2000@users.noreply.github.com>
2024-07-20 18:08:27 +0900

    more merge from latest develop

---
+++ test/util_test.dart
@@ -0,0 +1,17 @@
+// Package imports:
+import 'package:test/test.dart';
+
+// Project imports:
+import 'package:fnesemu/util.dart';
+
+void main() {
+  test('partial bit set ', () {
+    expect(0x321.withLowByte(0x5a), 0x35a);
+    expect((-1).withLowByte(0x5a), 0xffffffffffffff5a);
+  });
+
+  test('partial bit set ', () {
+    expect(0x321.withHighByte(0x5a), 0x5a21);
+    expect((-1).withHighByte(0x5a), 0xffffffffffff5aff);
+  });
+}
