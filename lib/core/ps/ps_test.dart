f16acbfd4034a70402ea18945784d1e9b9e20bed
reki2000 <2533597+reki2000@users.noreply.github.com>
2025-10-24 21:36:30 +0900

    ps: add test option

--- lib/core/ps/ps_test.dart
+++ lib/core/ps/ps_test.dart
@@ -3,7 +3,20 @@ import 'dart:io';
 import '../../disc/loader.dart';
 import 'ps.dart';
 
+int runSeconds = 10;
+
 main(List<String> args) {
+  if (args.length < 2) {
+    print(
+        "Usage: dart ps_test.dart [-n runSeconds] <bios file> <disc file> [<exe file>]");
+    return;
+  }
+
+  if (args[0] == "-n") {
+    runSeconds = int.parse(args[1]);
+    args = args.sublist(2);
+  }
+
   Ps core = Ps();
   final bios = File(args[0]).readAsBytesSync();
   core.setRom(bios);
