1134c725dbeb01c1f9f536242479ee6ca9115895
reki2000 <reki2000@users.noreply.github.com>
2026-06-27 02:29:39 +0000

    deploy: 18a39481969eff9eb7742b04eb9b97b5fdb7e01e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "508063042"
+    serviceWorkerVersion: "3910633046"
   }
 });
